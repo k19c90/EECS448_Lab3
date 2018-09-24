6bdde49778699842307b9a1860398352f852c74e
Kyle Curry <kylecurry1990@gmail.com>
2018-09-24 12:57:48 -0500

    exercise 4 now works

--- Exercise_4/exercise4.js
+++ Exercise_4/exercise4.js
@@ -3,7 +3,7 @@ function editBorderColor()
     let borderColor1 = document.getElementById("borderColor1").value;
     let borderColor2 = document.getElementById("borderColor2").value;
     let borderColor3 = document.getElementById("borderColor3").value;
-    document.getElementById("border").style.borderColor = "rgb(" + borderColor1 + "," + borderColor2 + "," + borderColor3")"
+    document.getElementById("border").style.borderColor = "rgb(" + borderColor1 + "," + borderColor2 + "," + borderColor3 + ")";
 }
 
 function editBackgroundColor()
@@ -11,7 +11,7 @@ function editBackgroundColor()
     let background1 = document.getElementById("backgroundColor1").value;
     let background2 = document.getElementById("backgroundColor2").value;
     let background3 = document.getElementById("backgroundColor3").value;
-    document.getElementById("border").style.backgroundColor = "rgb(" + background1 + "," + background2 + "," + background3")"
+    document.getElementById("border").style.backgroundColor = "rgb(" + background1 + "," + background2 + "," + background3 + ")";
 }
 
 function editBorderWidth()
