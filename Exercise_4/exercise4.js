68c5f3000e35678c0367494ee7b812015aa67785
Kyle Curry <kylecurry1990@gmail.com>
2018-09-24 12:41:03 -0500

    trying to get exercise 4 to work properly

--- Exercise_4/exercise4.js
+++ Exercise_4/exercise4.js
@@ -1,40 +1,23 @@
 function editBorderColor()
 {
-  let borderColor = document.getElementById("borderColor").value;
-  if((borderColor == "red" || "Red") || (borderColor == "green" || "Green") || (borderColor == "blue" || "Blue"))
-  {
-    document.getElementById("edit").style.borderColor=borderColor;
-  }
-  else
-  {
-    alert("Border must be red, green, or blue");
-  }
+    let borderColor1 = document.getElementById("borderColor1").value;
+    let borderColor2 = document.getElementById("borderColor2").value;
+    let borderColor3 = document.getElementById("borderColor3").value;
+    document.getElementById("border").style.borderColor = "rgb(" + borderColor1 + "," + borderColor2 + "," + borderColor3")"
 }
 
 function editBackgroundColor()
 {
-  let backgroud = document.getElementById("backgroundColor").value;
-  if((backgroud == "red" || "Red") || (backgroud == "green" || "Green") || (backgroud == "blue" || "Blue"))
-  {
-    document.getElementById("edit").style.backgroundColor=backgroud;
-  }
-  else
-  {
-    alert("Background must be red, green, or blue");
-  }
+    let background1 = document.getElementById("backgroundColor1").value;
+    let background2 = document.getElementById("backgroundColor2").value;
+    let background3 = document.getElementById("backgroundColor3").value;
+    document.getElementById("border").style.backgroundColor = "rgb(" + background1 + "," + background2 + "," + background3")"
 }
 
 function editBorderWidth()
 {
-  let borderWidth = document.getElementById("borderWidth").value;
-  if(isNaN(borderWidth))
-  {
-    alert("That's not a number");
-  }
-  else
-  {
-    document.getElementById("edit").style.borderWidth=borderWidth+'px';
-  }
+    let borderWidth = document.getElementById("borderWidth").value;
+    document.getElementById("border").style.borderWidth = borderWidth;
 }
 
 function editAll()
